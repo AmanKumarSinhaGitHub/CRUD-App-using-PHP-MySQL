bc80e000b8bdd88942df10ada3f42b8b3217b1ff
Aman Kumar Sinha <amankrsinha07@gmail.com>
2024-01-31 23:36:01 +0530

    Added index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>CRUD App using PHP MySQL</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+
+  <style>
+    html,
+    body {
+      background-color: gainsboro;
+    }
+  </style>
+
+  <div class="container py-5 px-5">
+
+    <div class="container text-center py-3">
+      <h2>CRUD App using PHP MySQL</h2>
+    </div>
+
+    <div class="container d-flex justify-content-center align-items-center py-5">
+      <a href="add-user.php" class="btn btn-primary">Add Users</a>
+    </div>
+
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
