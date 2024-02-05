200e615ce520fa1a9c42eefe605e5b7deb422330
Aman Kumar Sinha <amankrsinha07@gmail.com>
2024-02-05 21:52:47 +0530

    Added Delete Functionality

--- delete.php
+++ delete.php
@@ -5,16 +5,31 @@ if (isset($_GET["deleteID"])) {
   
   $id = $_GET['deleteID'];
 
-  // SQL query to delete the user record with the specified ID
-  $sql = "DELETE FROM userdetails WHERE `userdetails`.`id` = $id";
+  // Fetch the photo filename before deleting the user record
+  $getPhotoSql = "SELECT photo FROM userdetails WHERE id = $id";
+  $photoResult = mysqli_query($conn, $getPhotoSql);
 
-  // Execute the SQL query
-  $result = mysqli_query($conn, $sql);
+  if ($photoResult) {
+    $row = mysqli_fetch_assoc($photoResult);
+    $photoFilename = $row['photo'];
 
-  // Check if the query execution was successful
-  if ($result) {
-    header("location:display-user.php");
+    // Delete the user record
+    $deleteUserSql = "DELETE FROM userdetails WHERE id = $id";
+    $result = mysqli_query($conn, $deleteUserSql);
+
+    if ($result) {
+      // Delete the associated photo file
+      $photoPath = "uploads/" . $photoFilename;
+      if (file_exists($photoPath)) {
+        unlink($photoPath);
+      }
+
+      header("location:display-user.php");
+    } else {
+      die(mysqli_error($conn));
+    }
   } else {
     die(mysqli_error($conn));
   }
 }
+?>
